976bf5ed69d744b4ff2194e2938c1eb183d4cf62
devopscomputer <pauloti.engenharia@gmail.com>
2026-05-17 22:52:05 -0300

    chore: update artifacts/mockup-sandbox/vite.config.ts

--- artifacts/mockup-sandbox/vite.config.ts
+++ artifacts/mockup-sandbox/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import runtimeErrorOverlay from "@vite-plugin-runtime-error-modal";
 import { mockupPreviewPlugin } from "./mockupPreviewPlugin";
 
 const rawPort = process.env.PORT;
@@ -37,7 +37,7 @@ export default defineConfig({
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
+          await import("@vite-plugin-cartographer").then((m) =>
             m.cartographer({
               root: path.resolve(import.meta.dirname, ".."),
             }),
