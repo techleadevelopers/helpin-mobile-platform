1ea111b0c941e2496f5dc3418fe269ee0bb49119
unknown <pauloti.engenharia@gmail.com>
2026-09-09 10:04:42 -0300

    feat: atualizações gerais backend e mobile

--- backend/migrations/0012_push_radius_sniper.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE push_subscriptions
-  DROP CONSTRAINT IF EXISTS push_subscriptions_radius_km_check;
-
-ALTER TABLE push_subscriptions
-  DROP CONSTRAINT IF EXISTS push_subscriptions_radius_km_range;
-
-ALTER TABLE push_subscriptions
-  ADD CONSTRAINT push_subscriptions_radius_km_range
-  CHECK (radius_km BETWEEN 0.03 AND 50);
